1baf287571300b90e6c1509e96324ccaaa376cb9
Ivan Metta <ivanmetta@gmail.com>
2018-06-12 00:00:12 -0300

    se suben cambios de adaptacio de entrega 0 y 1 entre ambos grupos

---
+++ TPDDS-Grupo8/sge/src/main/java/dispositivo/Adaptador.java
@@ -0,0 +1,15 @@
+package dispositivo;
+
+public class Adaptador {
+private double id;
+
+public Adaptador() {}
+
+public double getId() {
+	return id;
+}
+
+public void setId(double id) {
+	this.id = id;
+}
+}
